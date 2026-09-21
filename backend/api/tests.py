5746897044854ed9c41d2e98de6b5e291d8b4425
Arnim-Zola <mohammedsahil0003@gmail.com>
2026-09-21 22:08:21 +0530

    test: add health check endpoint unit tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, Client
+class HealthTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+    def test_health_check(self):
+        res = self.client.get('/healthz')
+        self.assertEqual(res.status_code, 200)
+    def test_api_health_check(self):
+        res = self.client.get('/api/health/')
+        self.assertEqual(res.status_code, 200)
